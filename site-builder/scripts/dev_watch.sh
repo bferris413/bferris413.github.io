6e9170fcb1c18e29102b7e62594d99570f497a32
benjamin <bferris413@users.noreply.github.com>
2024-10-16 11:10:42 -0400

    Updated template, added dev support scripts.

---
+++ site-builder/scripts/dev_watch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+current_dir="$(dirname "$0")"
+cd "$current_dir" || exit 1
+
+cargo watch \
+    --watch-when-idle \
+    -x "run -- --template-file-path=$current_dir/../templates/index.html --out-file-path=$current_dir/../index.html --no-fetch --input-file=$current_dir/../templates/debug_commits.json" \
+    -w "$current_dir/../templates/index.html"
--- site-builder/scripts/run_tailwind.sh
+++ site-builder/scripts/run_tailwind.sh
@@ -3,4 +3,4 @@
 set -e
 
 cd "$(dirname "$0")"
-tailwindcss -c ../tailwind.config.js -i ../templates/tw.css -o ../templates/style.css --watch
+tailwindcss -c ../tailwind.config.js -i ../templates/tw.css -o ../style.css --watch
