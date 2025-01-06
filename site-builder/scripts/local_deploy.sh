fd0ff9e493116572ffd0e1e9f3ba3d87bf01b31b
benjamin <bferris413@users.noreply.github.com>
2025-01-06 18:40:39 -0500

    Expanded the site, included a little work history.

--- site-builder/scripts/local_deploy.sh
+++ site-builder/scripts/local_deploy.sh
@@ -13,9 +13,10 @@ cd ..
 pwd
 cargo build --release
 
-sudo cp target/release/site-builder /usr/local/bin/site-builder || true
+sudo cp target/release/site-builder /usr/local/bin/site-builder
 sudo mkdir -p /usr/local/bin/templates
 
-sudo cp templates/index.html /usr/local/bin/templates || true
-sudo cp templates/tw.css /usr/local/bin/templates || true
-sudo cp tailwind.config.js /usr/local/bin/templates || true
+sudo cp templates/index.html /usr/local/bin/templates
+sudo cp templates/tw.css /usr/local/bin/templates
+sudo cp -R templates/static /usr/local/bin/templates/static
+sudo cp tailwind.config.js /usr/local/bin/templates
