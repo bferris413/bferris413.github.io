b626bddfca378b32ed7a017cae7bed5b8f347e95
benjamin <bferris413@users.noreply.github.com>
2025-01-06 19:41:53 -0500

    Updated dev watch script.

--- site-builder/scripts/dev_watch.sh
+++ site-builder/scripts/dev_watch.sh
@@ -3,14 +3,14 @@
 current_dir="$(dirname "$0")"
 cd "$current_dir" || exit 1
 
-# cargo watch \
-#     --watch-when-idle \
-#     -x "run -- --template-file-path=$current_dir/../templates/index.html --out-file-path=$current_dir/../index.html" \
-#     -w "$current_dir/../templates/index.html" \
-#     -w "$current_dir/../src"
-
 cargo watch \
     --watch-when-idle \
-    -x "run -- --template-file-path=$current_dir/../templates/index.html --out-file-path=$current_dir/../index.html --no-fetch --input-file=$current_dir/../templates/debug_commits.json" \
+    -x "run -- --template-file-path=$current_dir/../templates/index.html --out-file-path=$current_dir/../index.html" \
     -w "$current_dir/../templates/index.html" \
     -w "$current_dir/../src"
+
+# cargo watch \
+#     --watch-when-idle \
+#     -x "run -- --template-file-path=$current_dir/../templates/index.html --out-file-path=$current_dir/../index.html --no-fetch --input-file=$current_dir/../templates/debug_commits.json" \
+#     -w "$current_dir/../templates/index.html" \
+#     -w "$current_dir/../src"
