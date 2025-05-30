f39be994f58c84f77c5aa1fd5d2a1fff0732e10c
benjamin <bferris413@users.noreply.github.com>
2025-05-30 13:51:31 -0400

    Updated templates dir

--- site-builder/scripts/local_deploy.sh
+++ site-builder/scripts/local_deploy.sh
@@ -17,6 +17,7 @@ sudo cp target/release/site-builder /usr/local/bin/site-builder
 sudo mkdir -p /usr/local/bin/templates
 
 sudo cp templates/index.html /usr/local/bin/templates
+sudo cp templates/post_template.html /usr/local/bin/templates
 sudo cp -R templates/posts /usr/local/bin/templates/posts
 sudo cp templates/tw.css /usr/local/bin/templates
 sudo cp -R templates/static /usr/local/bin/templates/static
